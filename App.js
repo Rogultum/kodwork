73467324170ed4e5723081afce9fefc9b2dc1b70
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-11-27 00:44:32 +0300

    add redux toolkit

--- App.js
+++ App.js
@@ -3,16 +3,19 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import FavouriteScreen from "./src/screens/FavouriteScreen";
 import JobScreen from "./src/screens/JobScreen";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Job" component={JobScreen} />
-        <Drawer.Screen name="Favourite" component={FavouriteScreen} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Provider>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Job" component={JobScreen} />
+          <Drawer.Screen name="Favourite" component={FavouriteScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
