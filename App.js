e0621f7a8ac96916d1aa357dcfe983ef6fdb3b64
Remzi Ogul TUM <amadeuwoles@gmail.com>
2022-12-01 18:03:01 +0300

    add stack nav + store

--- App.js
+++ App.js
@@ -4,16 +4,25 @@ import FavouriteScreen from "./src/screens/FavouriteScreen";
 import JobScreen from "./src/screens/JobScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-
+import { store } from "./src/redux/store";
+import StackNavigation from "./src/navigation/StackNavigation";
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <NavigationContainer>
         <Drawer.Navigator>
-          <Drawer.Screen name="Job" component={JobScreen} />
-          <Drawer.Screen name="Favourite" component={FavouriteScreen} />
+          <Drawer.Screen
+            name="Job"
+            component={StackNavigation}
+            options={{ headerShown: false }}
+          />
+          <Drawer.Screen
+            name="Favourite"
+            component={FavouriteScreen}
+            options={{ headerTintColor: "tomato" }}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     </Provider>
